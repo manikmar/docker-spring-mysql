22740c9b9b52cb41118fa80984d36728dc989665
Mani <manikumar.panithi@gmail.com>
2023-03-21 23:40:01 +0530

    first commit for docker of spring boot and mysql

---
+++ src/main/java/com/acc/dockerspringmysql/DockerSpringMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.acc.dockerspringmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringMysqlApplication.class, args);
+	}
+
+}
